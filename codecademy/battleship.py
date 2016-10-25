d1ef8ef27cfc7321428e3a03645146dc71fecd2a
JessaWitzel <jessa@riotousliving.com>
2016-10-25 19:14:38 -0400

    Add files via upload

--- codecademy/battleship.py
+++ codecademy/battleship.py
@@ -1,36 +1,50 @@
-#terminal battleship game
+#my version of the Codecademy Battleship exercise where you can have as many turns as you want
 
 from random import randint
 
-game_board = []
-for x in range(0,5):
-    game_board.append(['0'] * 5)
-
+board = []
+#creates a list of lists of 5 '0's
+for x in range(0, 5):
+    board.append(["O"] * 5)
+#joins each list of 0s into a string and prints on a new line
 def print_board(board):
     for row in board:
-        print ' '.join(row)
+        print " ".join(row)
+#defines where the battleship is, row and column
+def random_row(board):
+    return randint(0, len(board) - 1)
 
-def game():
-    turns = int(raw_input('How many turns would you like? '))
-    for x in range(0,turns):
-        print "This is turn: " + str(x + 1)
-        print_board(game_board)
-        print "Tell us what row and column you think the ship is hiding in."
-        guess_row = int(raw_input("Row: "))
-        guess_col = int(raw_input("Column: "))
-        battle(guess_row, guess_col, x)
+def random_col(board):
+    return randint(0, len(board[0]) - 1)
 
-ship_row = randint(1,5)
-ship_col = randint(1,5)
+ship_row = random_row(board)
+ship_col = random_col(board)
 
-def battle(row, column, x):
+def play(gameplay):
+    for turn in range(gameplay):
+        print "Turn " + str(turn + 1)
+        guess_row = int(raw_input("Guess Row:"))
+        guess_col = int(raw_input("Guess Col:"))
+        guesses(guess_row, guess_col, turn, gameplay)
+
+def guesses(row, column, turn, gameplay):
     if row == ship_row and column == ship_col:
-        print "You sunk my battleship!"
+        print "Congratulations! You sank my battleship"
         exit()
-    elif game_board[row - 1][column - 1] == '0':
-        print "There is no ship there."
-        game_board[row - 1][column - 1] = 'X'
-    elif game_board[row - 1][column - 1] == 'X':
-        print "You already guessed that. Try again!"
+    elif row not in range(5) or column not in range(5):
+            print "Oops, that's not even in the ocean."
+    elif board[row][column] == "X":
+            print "You guessed that one already."
+    else:
+        print "You missed my battleship!"
+        board[row][column] = "X"
+        print_board(board)
+        if turn == gameplay - 1:
+            print "Game Over"
+        else:
+            pass
 
-game()
+wanted_turns = int(raw_input("How many turns do you want?: "))
+print_board(board)
+play(wanted_turns)
+# Ideas to keep working on: 2 player game; multiple ships; multiple size ships
